7c2efeeeed76b81b092385b3c252c68f060ab22d
Rodrigue Nyassa <nyassarodrigue3@gmail.com>
2026-07-16 10:56:52 +0100

    fix_depoyment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,18 +20,13 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
-
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    <html className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Providers> 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
-import Experience from "@/components/Exprerience";
+import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
