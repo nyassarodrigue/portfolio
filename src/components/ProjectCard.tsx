38bccf67626eee6045854aa70d0dfddb48c70dff
Rodrigue Nyassa <nyassarodrigue3@gmail.com>
2026-07-16 11:18:07 +0100

    Add Project type

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import {Project} from "@/type/Project";
+import type { Project } from "@/type/Project";
 
 interface Props {
   project: Project;
