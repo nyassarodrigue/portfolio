1c6895e323cb727306864d3e1989affeb500f62c
Rodrigue Nyassa <nyassarodrigue3@gmail.com>
2026-07-17 14:23:32 +0100

    Ajout du nombre de visites

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Analytics} from "@vercel/analytics/next";
 import Providers from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
@@ -75,7 +76,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Analytics />
+        </Providers>
       </body>
     </html>
   );
